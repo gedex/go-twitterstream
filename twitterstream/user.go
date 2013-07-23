ac4f8c6f83755f17e1a06194f4a270f01fad0ed0
Akeda Bagus <akeda@x-team.com>
2013-07-23 21:24:10 +0700

    Added user endpoints call.

--- twitterstream/user.go
+++ twitterstream/user.go
@@ -5,6 +5,46 @@
 
 package twitterstream
 
+import (
+	"net/url"
+)
+
 type UserStreams struct {
 	client *Client
+}
+
+func (s *UserStreams) Get(f map[string]string) error {
+	baseURL, _ := url.Parse("https://userstream.twitter.com/1.1/")
+	s.client.baseURL = baseURL
+
+	u := "user.json"
+
+	params := url.Values{
+		"stall_warnings": {"true"},
+	}
+	if v, exists := f["with"]; exists {
+		params.Add("with", v)
+	}
+	if v, exists := f["replies"]; exists {
+		params.Add("replies", v)
+	}
+	if v, exists := f["track"]; exists {
+		params.Add("track", v)
+	}
+	if v, exists := f["locations"]; exists {
+		params.Add("locations", v)
+	}
+	u += "?" + params.Encode()
+
+	req, err := s.client.NewRequest("GET", u, nil)
+	if err != nil {
+		return err
+	}
+
+	resp, err := s.client.Do(req)
+	if err != nil {
+		return err
+	}
+
+	return s.client.DispatchResponse(resp)
 }
