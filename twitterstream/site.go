5bc62d9d6a320355ba0461a672d446fa5d42d20b
Akeda Bagus <akeda@x-team.com>
2013-07-25 23:47:47 +0700

    Added site stream service.

--- twitterstream/site.go
+++ twitterstream/site.go
@@ -5,6 +5,38 @@
 
 package twitterstream
 
+import (
+	"net/url"
+)
+
 type SiteStreams struct {
 	client *Client
+}
+
+func (s *SiteStreams) Get(f map[string]string) error {
+	baseURL, _ := url.Parse("https://sitestream.twitter.com/1.1/")
+	s.client.baseURL = baseURL
+
+	u := "site.json"
+
+	params := []string{"follow", "with", "replies"}
+	body := make(map[string]string)
+	for _, p := range params {
+		if v, exists := f[p]; exists && v != "" {
+			body[p] = v
+		}
+	}
+	body["stall_warnings"] = "true"
+
+	req, err := s.client.NewRequest("POST", u, body)
+	if err != nil {
+		return err
+	}
+
+	resp, err := s.client.Do(req)
+	if err != nil {
+		return err
+	}
+
+	return s.client.DispatchResponse(resp)
 }
